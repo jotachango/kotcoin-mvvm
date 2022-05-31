000726e48def85ced0833311ab84b631739d5c56
jnfran92 <jnfran92@gmail.com>
2022-05-31 16:52:41 -0400

    compose init

--- app/src/main/java/com/jnfran92/kotcoin/dashboard/ui/DashboardActivity.kt
+++ app/src/main/java/com/jnfran92/kotcoin/dashboard/ui/DashboardActivity.kt
@@ -3,9 +3,14 @@ package com.jnfran92.kotcoin.dashboard.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.jnfran92.kotcoin.R
 
 class DashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +24,20 @@ class DashboardActivity : ComponentActivity() {
 
 @Composable
 fun SimpleComposable() {
-    Text("Hello World")
+    Row{
+        Image(
+            painter = painterResource(R.drawable.ic_baseline_attach_money_24),
+            contentDescription = "Contact profile picture",
+        )
+        Column {
+            Text("Hello World 1")
+            Text("Hello World 2")
+        }
+        Column {
+            Text("Hello")
+            Text("Hello Thor")
+        }
+    }
 }
 
 @Preview
