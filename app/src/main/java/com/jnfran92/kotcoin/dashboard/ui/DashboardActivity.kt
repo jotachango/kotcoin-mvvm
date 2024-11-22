9c96a3775d7a9259a174296530a8bd65520f0ddd
Juan Francisco Chango <jfchangop@falabella.cl>
2024-11-22 14:26:16 -0500

    theme Material 3 implemenbted!

--- app/src/main/java/com/jnfran92/kotcoin/dashboard/ui/DashboardActivity.kt
+++ app/src/main/java/com/jnfran92/kotcoin/dashboard/ui/DashboardActivity.kt
@@ -4,12 +4,22 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.jnfran92.kotcoin.common.ui.theme.KotcoinAppTheme
 import com.jnfran92.kotcoin.dashboard.presentation.model.UIDashboard
@@ -19,7 +29,7 @@ class DashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            KotcoinAppTheme {
                 DashboardView()
             }
         }
@@ -29,12 +39,19 @@ class DashboardActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardView() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
     Scaffold(
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
                 title = {
-                    Text("Kotcoin")
-                }
+                    Text(
+                        "Kotcoin",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                scrollBehavior = scrollBehavior,
             )
         },
     ) { innerPadding ->
@@ -44,10 +61,10 @@ fun DashboardView() {
         )
     }
 }
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL)
+@Preview
 @Composable
 fun ComposablePreview() {
-    KotcoinAppTheme {
+    KotcoinAppTheme() {
         DashboardView()
     }
 }
